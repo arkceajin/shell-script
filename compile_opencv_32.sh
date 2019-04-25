5cdc2996c736bf9a2af63377bd93c1f6f9ddd2d2
arc <arkceajin@gmail.com>
2019-04-25 18:56:24 +0900

    Update compile_opencv_32.sh

--- compile_opencv_32.sh
+++ compile_opencv_32.sh
@@ -1,6 +1,7 @@
 if ! [ -x "$(command -v cmake)" ]; then
   cd /opt && sudo wget https://github.com/Kitware/CMake/releases/download/v3.5.1/cmake-3.5.1-Linux-i386.sh
   sudo chmod a+x cmake-3.5.1-Linux-i386.sh
+  sudo mkdir -p /opt/cmake-3.5.1-Linux-i386
   sudo bash cmake-3.5.1-Linux-i386.sh --skip-license --prefix=/opt/cmake-3.5.1-Linux-i386
   sudo ln -s /opt/cmake-3.5.1-Linux-i386/bin/* /usr/local/bin
 fi
@@ -9,9 +10,9 @@ if [ ! -d "/opt/opencv-4.0.1" ]; then
   cd /opt && wget https://github.com/opencv/opencv/archive/4.0.1.zip
   sudo apt-get install unzip
   unzip 4.0.1.zip
-  #sed -i 's/#define CV__EXCEPTION_PTR/#undef CV__EXCEPTION_PTR\n#define CV__EXCEPTION_PTR/' opencv-4.0.1/modules/core/src/parallel.cpp
+  mkdir -p /opt/opencv-4.0.1/build
 fi
 
-cd /opt/opencv-4.0.1 && mkdir build && cd build
+cd /opt/opencv-4.0.1/build
 cmake -DCMAKE_TOOLCHAIN_FILE=../platforms/linux/arm-gnueabi.toolchain.cmake -DCMAKE_INSTALL_PREFIX=~/opencv4-static -DBUILD_SHARED_LIBS=OFF -DBUILD_WITH_STATIC_CRT=ON -DCMAKE_BUILD_TYPE=RELEASE -DOPENCV_GENERATE_PKGCONFIG=ON -DWITH_OPENEXR=OFF ../
 make -j$(nproc) && make install
