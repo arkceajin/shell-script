3a806d7fdc8cc086af6e8e21f0c2ae553ea2a7c6
arc <arkceajin@gmail.com>
2019-04-26 11:12:55 +0900

    Update compile_opencv.sh

--- compile_opencv.sh
+++ compile_opencv.sh
@@ -1,20 +1,45 @@
+WORK_DIR="/opt"
+OPENCV_PREFIX="/usr/opencv4-static"
+OPENCV_VERSION="4.0.1"
+OPENCV_SRC="${WORK_DIR}/opencv-${OPENCV_VERSION}"
+MACHINE_TYPE=""
+if [ $(uname -m) == 'x86_64' ]; then
+  # 64-bit
+  MACHINE_TYPE="x86_64"
+else
+  # 32-bit
+  MACHINE_TYPE="i386"
+fi
+CMAKE_VERSION="3.5.1"
+CMAKE_PKG="cmake-${CMAKE_VERSION}-Linux-${MACHINE_TYPE}"
+
+# install cmake
 if ! [ -x "$(command -v cmake)" ]; then
-  cd /opt && sudo wget https://github.com/Kitware/CMake/releases/download/v3.5.1/cmake-3.5.1-Linux-x86_64.sh
-  sudo chmod a+x cmake-3.5.1-Linux-x86_64.sh
-  sudo mkdir -p /opt/cmake-3.5.1-Linux-x86_64
-  sudo bash /opt/cmake-3.5.1-Linux-x86_64.sh --skip-license --prefix=/opt/cmake-3.5.1-Linux-x86_64
-  sudo ln -s /opt/cmake-3.5.1-Linux-x86_64/bin/* /usr/local/bin
+  cd ${WORK_DIR} && sudo wget "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_PKG}.sh"
+  sudo chmod a+x "${CMAKE_PKG}.sh"
+  sudo mkdir -p "${WORK_DIR}/${CMAKE_PKG}"
+  sudo bash "${WORK_DIR}/${CMAKE_PKG}.sh" --skip-license --prefix="${WORK_DIR}/${CMAKE_PKG}"
+  sudo ln -s "${WORK_DIR}/${CMAKE_PKG}/bin/*" /usr/local/bin
 fi
 
-if [ ! -d "/opt/opencv-4.0.1" ]; then
-  cd /opt && wget https://github.com/opencv/opencv/archive/4.0.1.zip
+# install opencv
+if [ ! -d ${OPENCV_SRC} ]; then
+  cd ${WORK_DIR} && wget "https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip"
   sudo apt-get install unzip
-  unzip 4.0.1.zip
-  sudo sed -i 's/#ifndef CV__EXCEPTION_PTR/#undef CV__EXCEPTION_PTR\n#ifndef CV__EXCEPTION_PTR/' /opt/opencv-4.0.1/modules/core/src/parallel.cpp
-  mkdir -p /opt/opencv-4.0.1/build
+  unzip "${OPENCV_VERSION}.zip"
+  if [ $(uname -m) == 'x86_64' ]; then
+    sudo sed -i 's/#ifndef CV__EXCEPTION_PTR/#undef CV__EXCEPTION_PTR\n#ifndef CV__EXCEPTION_PTR/' "${OPENCV_SRC}/modules/core/src/parallel.cpp"
+  fi
+  mkdir -p "${OPENCV_SRC}/build"
 fi
 
-cd /opt/opencv-4.0.1/build
-cmake -DCMAKE_C_COMPILER=arm-linux-gnueabi-gcc-4.9 -DCMAKE_CXX_COMPILER=arm-linux-gnueabi-g++-4.9 -DBUILD_TIFF=ON -DCMAKE_INSTALL_PREFIX=/usr/arm-linux-gnueabi/opencv4 -DBUILD_SHARED_LIBS=OFF -DINSTALL_PYTHON_EXAMPLES=OFF -DBUILD_NEW_PYTHON_SUPPORT=OFF -DWITH_PROTOBUF=OFF -DWITH_ADE=OFF -DWITH_CUDA=OFF -DWITH_IPP=OFF -DWITH_QT=OFF -DWITH_GTK=OFF -DWITH_OPENEXR=OFF -DOPENCV_GENERATE_PKGCONFIG=ON ../
-#cmake -DCMAKE_C_COMPILER=arm-linux-gnueabi-gcc-4.9 -DCMAKE_CXX_COMPILER=arm-linux-gnueabi-g++-4.9 -DCMAKE_INSTALL_PREFIX=/usr/opencv4 -DWITH_PROTOBUF=OFF -DWITH_ADE=OFF -DWITH_CUDA=OFF -DBUILD_TIFF=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_WITH_STATIC_CRT=ON -DCMAKE_BUILD_TYPE=RELEASE -DOPENCV_GENERATE_PKGCONFIG=ON -DWITH_OPENEXR=OFF ../
+# start build opencv
+cd "${OPENCV_SRC}/build"
+if [ $(uname -m) == 'x86_64' ]; then
+  # 64-bit 
+  cmake -DCMAKE_C_COMPILER=arm-linux-gnueabi-gcc-4.9 -DCMAKE_CXX_COMPILER=arm-linux-gnueabi-g++-4.9 -DBUILD_TIFF=ON -DCMAKE_INSTALL_PREFIX=${OPENCV_PREFIX} -DBUILD_SHARED_LIBS=OFF -DINSTALL_PYTHON_EXAMPLES=OFF -DBUILD_NEW_PYTHON_SUPPORT=OFF -DWITH_PROTOBUF=OFF -DWITH_ADE=OFF -DWITH_CUDA=OFF -DWITH_IPP=OFF -DWITH_QT=OFF -DWITH_GTK=OFF -DWITH_OPENEXR=OFF -DOPENCV_GENERATE_PKGCONFIG=ON ../
+else
+  # 32-bit 
+  cmake -DCMAKE_TOOLCHAIN_FILE=../platforms/linux/arm-gnueabi.toolchain.cmake -DCMAKE_INSTALL_PREFIX=${OPENCV_PREFIX} -DBUILD_SHARED_LIBS=OFF -DBUILD_WITH_STATIC_CRT=ON -DCMAKE_BUILD_TYPE=RELEASE -DOPENCV_GENERATE_PKGCONFIG=ON -DWITH_OPENEXR=OFF ../
+fi
 make -j$(nproc) && make install
